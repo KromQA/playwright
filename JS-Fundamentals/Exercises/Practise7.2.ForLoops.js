171bcb8e39a6de3da12c6c0f2898cdef8cdacf70
KromQA <kromski.k@gmail.com>
2026-01-05 14:00:14 +0000

    New For Loop practise

---
+++ JS-Fundamentals/Exercises/Practise7.2.ForLoops.js
@@ -0,0 +1,30 @@
+// another For Loop practise
+
+// TASK 1:
+// Print numbers from 1 to 5
+for (let Task1 = 1; Task1 < 6; Task1++) {
+    console.log()
+}
+// TASK 2:
+// Print numbers from 5 to 1
+for (let Task2 = 5; Task2 > 0; Task2--)
+    console.log(Task2)
+
+// TASK 3:
+// Print numbers from 0 to 10
+for (let Task3 = 0; Task3 < 11; Task3++) {
+    console.log(Task3)
+}
+
+// TASK 4:
+// Print "Hello" 3 times
+for (let Task4 = 0; Task4 < 3; Task4++) {
+    console.log('Hello')
+}
+
+// TASK 5:
+// Given an array of colors, print each color
+var colours = ["red", "green", "blue"]
+for (let Task5 = 0; Task5 < 3; Task5++) {
+    console.log(colours[Task5])
+}
